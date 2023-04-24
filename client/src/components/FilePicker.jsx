a5cb98b71b5e182b6c17087a9144108a15482aac
Bryan W <bhoff1980@gmail.com>
2023-04-24 16:11:45 -0600

    buttons are setup for image uploader

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -11,9 +11,30 @@ const FilePicker = ({file, setFile, readFile}) => {
       accept='image/*'
       onChange={(e) => setFile(e.target.files[0])}
       />
-    <label htmlFor='file-upload'>
+    <label htmlFor='file-upload' className='filepicker-label'>
       Upload File
     </label>
+
+    <p className='mt-2 text-gray-500 text-xs truncate'>
+      {file === '' ? "No File Selected" : file.name}
+    </p>
+     </div>
+     <div className='mt-4 flex flex-wrap gap-3'>
+      <CustomButton 
+      type='outline'
+      title='logo'
+      handleClick={() => readFile('logo')}
+      customStyles='text-xs'
+
+      />
+          <CustomButton 
+      type='filled'
+      title='Full'
+      handleClick={() => readFile('full')}
+      customStyles='text-xs'
+
+      />
+
      </div>
     </div>
   )
