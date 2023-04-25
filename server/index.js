eecb17c816edb592d170c76f83f0a35a1fced6a6
Bryan W <bhoff1980@gmail.com>
2023-04-24 19:44:12 -0600

    route created to get created image from openAI

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
+import dalleRoutes from './routes/dalleRoutes.js'
 
 dotenv.config()
 
@@ -8,8 +9,12 @@ const app = express()
 app.use(cors())
 app.use(express.json({limig: '50mb'}))
 
+app.use('/api/v1/dalle', dalleRoutes)
+
 app.get('/', (req, res) => {
     res.status(200).json({message:'hello from dall-e'})
 })
+
+
 
 app.listen(5000, () => console.log("Server has Started at port 5000"))
